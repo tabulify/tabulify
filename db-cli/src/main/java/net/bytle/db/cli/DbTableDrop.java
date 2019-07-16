85bea911a397342f8ce552d418f29ebd79c867c4
Nicolas GERARD <gerardnico@gmail.com>
2019-07-16 15:09:54 +0200

    Backup - Fill table

--- db-cli/src/main/java/net/bytle/db/cli/DbTableDrop.java
+++ db-cli/src/main/java/net/bytle/db/cli/DbTableDrop.java
@@ -25,7 +25,7 @@ import static net.bytle.db.cli.DbDatabase.STORAGE_PATH;
 public class DbTableDrop {
 
     private static final Log LOGGER = Db.LOGGER_DB_CLI;
-    public static final String NO_STRICT = "nostrict";
+    public static final String NO_STRICT = "no-strict";
     public static final String FORCE = "force";
     private static final String TABLE_URIS = "tableUri...";
 
