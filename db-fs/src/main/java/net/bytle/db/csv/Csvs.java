7e46d62cda524704ef52db8032d09a687f685fb4
gerardnico <gerardnico@gmail.com>
2019-11-15 16:49:14 +0100

    Backup Csv - yeah - safe loop was easier than that

--- db-fs/src/main/java/net/bytle/db/csv/Csvs.java
+++ db-fs/src/main/java/net/bytle/db/csv/Csvs.java
@@ -12,27 +12,17 @@ import java.util.NoSuchElementException;
 public class Csvs {
 
     /**
-     *
      * @param recordIterator
      * @param csvDataPath
      * @return a csvRecord or null if this is the end
      */
     public static CSVRecord safeIterate(Iterator<CSVRecord> recordIterator, CsvDataPath csvDataPath) {
-        try {
+
+        if (recordIterator.hasNext()) {
             return recordIterator.next();
-        } catch (NoSuchElementException e) {
+        } else {
             return null;
-        } catch (Exception e) {
-            if (e instanceof IllegalStateException){
-                // We got that when the file is empty
-                if (Fs.isEmpty(csvDataPath.getNioPath())) {
-                    return null;
-                } else {
-                    throw new RuntimeException("IllegalStateException: Error when iterating on the next csv record", e);
-                }
-            } else {
-                throw new RuntimeException("Error when iterating on the next csv record", e);
-            }
         }
+
     }
 }
