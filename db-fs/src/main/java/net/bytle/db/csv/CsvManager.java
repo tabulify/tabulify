50b28d8a0bc31d5e155f76f2e8cfb5dad56a5a2a
gerardnico <gerardnico@gmail.com>
2019-11-28 15:59:48 +0100

    Tpcsds backup

--- db-fs/src/main/java/net/bytle/db/csv/CsvManager.java
+++ db-fs/src/main/java/net/bytle/db/csv/CsvManager.java
@@ -1,17 +1,20 @@
 package net.bytle.db.csv;
 
-import net.bytle.db.fs.FsDataPath;
 import net.bytle.db.model.ColumnDef;
-import net.bytle.db.spi.DataPath;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVPrinter;
 
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
 
 public class CsvManager {
     public static void create(CsvDataPath csvDataPath) {
+
+        try {
+            Files.createFile(csvDataPath.getNioPath());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
         CsvDataDef csvDataDef = csvDataPath.getDataDef();
         CSVFormat csvFormat = csvDataPath.getDataDef().getCsvFormat();
@@ -20,7 +23,6 @@ public class CsvManager {
                     .map(ColumnDef::getColumnName).toArray(String[]::new);
             if (headers.length!=0) {
                 csvFormat = csvFormat.withHeader(headers);
-
             } else {
                 throw new RuntimeException("The CSV file has a format asking to print the headers but there is no columns defined for this CSV");
             }
