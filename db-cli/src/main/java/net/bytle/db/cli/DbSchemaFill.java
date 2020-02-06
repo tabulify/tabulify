a7db5e2be1a15f07d1907679367f3c54eed33e47
gerardnico <gerardnico@gmail.com>
2020-02-06 14:40:34 +0100

    Db Schema fill done

--- db-cli/src/main/java/net/bytle/db/cli/DbSchemaFill.java
+++ db-cli/src/main/java/net/bytle/db/cli/DbSchemaFill.java
@@ -6,14 +6,10 @@ import net.bytle.cli.CliParser;
 import net.bytle.cli.CliUsage;
 import net.bytle.cli.Clis;
 import net.bytle.db.Tabular;
-import net.bytle.db.database.Database;
-import net.bytle.db.engine.ForeignKeyDag;
-import net.bytle.db.engine.Tables;
 import net.bytle.db.gen.DataGeneration;
 import net.bytle.db.model.DataDefs;
-import net.bytle.db.model.ForeignKeyDef;
-import net.bytle.db.model.TableDef;
 import net.bytle.db.spi.DataPath;
+import net.bytle.db.spi.Tabulars;
 import net.bytle.fs.Fs;
 import net.bytle.timer.Timer;
 import org.slf4j.Logger;
@@ -22,6 +18,8 @@ import org.slf4j.LoggerFactory;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 import static net.bytle.db.cli.Words.DATASTORE_VAULT_PATH;
@@ -36,8 +34,9 @@ import static net.bytle.db.cli.Words.NOT_STRICT;
 public class DbSchemaFill {
 
   private static final Logger LOGGER = LoggerFactory.getLogger(DbSchemaFill.class);
-  ;
+
   protected final static String SCHEMA_URI = "SchemaUri";
+  protected final static String WITH_DEPENDENCIES = "with-dependencies";
 
 
   public static void run(CliCommand cliCommand, String[] args) {
@@ -53,24 +52,28 @@ public class DbSchemaFill {
     cliCommand.flagOf(NOT_STRICT)
       .setDescription("if set, it will not throw an error for minor problem (example if a data def has not a yml extension,...) ")
       .setDefaultValue(false);
+    cliCommand.flagOf(WITH_DEPENDENCIES)
+      .setDescription("if set, it will load also the dependencies (ie foreign tables)")
+      .setDefaultValue(false);
     cliCommand.optionOf(DATASTORE_VAULT_PATH);
 
     // Parser and args
     CliParser cliParser = Clis.getParser(cliCommand, args);
-    final String dataDefGlob = cliParser.getString(Words.GLOB_PATERN_DATADEF_FILE);
-    final Boolean notStrictRun = cliParser.getBoolean(NOT_STRICT);
-    final Path storagePathValue = cliParser.getPath(DATASTORE_VAULT_PATH);
-    final String schemaUri = cliParser.getString(SCHEMA_URI);
+    final String dataDefGlobArg = cliParser.getString(Words.GLOB_PATERN_DATADEF_FILE);
+    final Boolean notStrictRunArg = cliParser.getBoolean(NOT_STRICT);
+    final Boolean withDependencies = cliParser.getBoolean(WITH_DEPENDENCIES);
+    final Path storagePathValueArg = cliParser.getPath(DATASTORE_VAULT_PATH);
+    final String targetSchemaDataUriArg = cliParser.getString(SCHEMA_URI);
 
     // Args control
     List<Path> dataDefsPaths = new ArrayList<>();
-    if (dataDefGlob != null) {
-      dataDefsPaths = Fs.getFilesByGlob(dataDefGlob);
+    if (dataDefGlobArg != null) {
+      dataDefsPaths = Fs.getFilesByGlob(dataDefGlobArg);
       dataDefsPaths.forEach(yamlFile -> {
         String fileExtension = Fs.getExtension(yamlFile.getFileName().toString());
         if (!fileExtension.toLowerCase().equals("yml")) {
           String msg = "A definition file must have a yml extension. The file (" + yamlFile + ") has not a yml extension.";
-          if (notStrictRun) {
+          if (notStrictRunArg) {
             LOGGER.warn(msg);
           } else {
             LOGGER.error(msg);
@@ -85,77 +88,96 @@ public class DbSchemaFill {
     // Main
     try (Tabular tabular = Tabular.tabular()) {
 
-      if (storagePathValue != null) {
-        tabular.setDataStoreVault(storagePathValue);
+      if (storagePathValueArg != null) {
+        tabular.setDataStoreVault(storagePathValueArg);
       } else {
         tabular.withDefaultStorage();
       }
 
-      List<DataPath> dataPaths = DataDefs.load(dataGenYml)
-        .stream()
-        .map(dataDefDataPath -> {
-          final JdbcDataPath childDataPath = jdbcDataSystem.getDataPath(dataDefDataPath.getName());
-          DataDefs.mergeProperties(childDataPath.getDataDef(), dataDefDataPath.getDataDef());
-          return childDataPath;
-        })
-        .collect(Collectors.toList());
-
-      Timer cliTimer = Timer.getTimer("schema").start();
-
-      if (yamlFile != null) {
-
-        LOGGER.info("Loading generated data from the path (" + yamlFile + ")");
-        List<TableDef> dataGenDefs = DataDefs.of().load(yamlFile);
-
-        List<DataPath> tables = DataGeneration.of()
-          .addTables(dataGenDefs)
-          .loadDependencies(true)
-          .load();
-
-        LOGGER.info("The following tables where loaded:");
-        for (TableDef tableDef : tables) {
-          LOGGER.info("  * " + tableDef.getFullyQualifiedName() + ", Size (" + Tables.getSize(tableDef) + ")");
-        }
-
-      } else {
-
-        SchemaDef currentSchema = database.getCurrentSchema();
-        LOGGER.info("Loading generated data to the schema (" + currentSchema.getName() + ") without file definition.");
-
-        List<TableDef> tableDefs = ForeignKeyDag.get(currentSchema).getCreateOrderedTables();
-        if (tableDefs.size() > 0) {
-
-          // List
-          for (TableDef tableDef : tableDefs) {
-            List<ForeignKeyDef> foreignKeys = tableDef.getForeignKeys();
-            String parentTableDef = "no parent";
-            if (foreignKeys.size() > 0) {
-              List<String> foreignPrimaryKeys = new ArrayList<>();
-              for (ForeignKeyDef foreignKeyDef : foreignKeys) {
-                foreignPrimaryKeys.add(foreignKeyDef.getForeignPrimaryKey().getTableDef().getName());
-              }
-              parentTableDef = String.join(",", foreignPrimaryKeys) + " as parent";
-            }
-            LOGGER.fine(tableDef.getFullyQualifiedName() + " has " + parentTableDef);
-          }
-
-          // Loading
-          for (TableDef tableDef : tableDefs) {
-            DataGeneration.of()
-              .addTable(tableDef)
-              .load();
-          }
+      // Target
+      DataPath targetDataPath = tabular.getDataPath(targetSchemaDataUriArg);
+      if (!Tabulars.isContainer(targetDataPath)) {
+        String msg = "The target data uri (" + targetSchemaDataUriArg + " is not a schema";
+        if (notStrictRunArg) {
+          LOGGER.warn(msg);
         } else {
-          LOGGER.warning("No table to load in the schema (" + currentSchema + ")");
+          LOGGER.error(msg);
+          System.exit(1);
         }
       }
+
+      List<DataPath> dataPaths = Tabulars.getChildren(targetDataPath);
+      if (dataPaths.size() == 0) {
+        String msg = "The target schema defined by the data uri (" + targetSchemaDataUriArg + ") seems to have no data paths (tables)";
+        if (notStrictRunArg) {
+          LOGGER.warn(msg);
+          System.exit(0);
+        } else {
+          LOGGER.error(msg);
+          System.exit(1);
+        }
+      }
+
+      List<Path> dataGenYmls = null;
+      if (dataDefGlobArg != null) {
+
+        dataGenYmls = Fs.getFilesByGlob(dataDefGlobArg);
+        if (dataGenYmls.size() == 0) {
+          String msg = "The data def glob pattern (" + dataDefGlobArg + ") has selected no data definition files";
+          if (notStrictRunArg) {
+            LOGGER.warn(msg);
+          } else {
+            LOGGER.error(msg);
+            System.exit(1);
+          }
+        } else {
+
+          Map<String, DataPath> dataPathDataGen = dataGenYmls
+            .stream()
+            .map(tabular::getDataPathOfDataDef)
+            .collect(Collectors.toMap(DataPath::getName, Function.identity()));
+
+          // Merge
+          dataPaths.stream()
+            .filter(dp -> dataPathDataGen.keySet().contains(dp.getName()))
+            .forEach(dp -> {
+                DataDefs.mergeProperties(dp.getDataDef(), dataPathDataGen.get(dp.getName()).getDataDef());
+              }
+            );
+
+        }
+      }
+
+      Timer cliTimer = Timer.getTimer("schema fill").start();
+
+      // Data Generation object build
+      DataGeneration dataGeneration = DataGeneration.of()
+        .addTables(dataPaths);
+
+      // Feedback message building
+      String withOrWithoutDependencies = "without";
+      if (withDependencies) {
+        dataGeneration.loadDependencies(true);
+        withOrWithoutDependencies = "and";
+      }
+      String withOrWithoutDataDef = "without data definitions ";
+      if (dataGenYmls!=null){
+        withOrWithoutDataDef="with ("+dataGenYmls.size()+") data definitions defined by the glob pattern (" + dataDefGlobArg + ") ";
+      }
+      LOGGER.info("Loading generated data "+withOrWithoutDataDef + withOrWithoutDependencies + " the dependencies (foreign tables)");
+      List<DataPath> loadedDataPaths = dataGeneration.load();
 
       cliTimer.stop();
       LOGGER.info("Response Time for the loading of generated data : " + cliTimer.getResponseTime() + " (hour:minutes:seconds:milli)");
       LOGGER.info("       Ie (" + cliTimer.getResponseTimeInMilliSeconds() + ") milliseconds%n");
       LOGGER.info("Success ! No errors were seen.");
 
+      LOGGER.info("The following tables where loaded:");
+      for (DataPath dataPath : loadedDataPaths) {
+        LOGGER.info("  * " + dataPath + ", Size (" + Tabulars.getSize(dataPath) + ")");
+      }
+
     }
 
-
   }
+}
