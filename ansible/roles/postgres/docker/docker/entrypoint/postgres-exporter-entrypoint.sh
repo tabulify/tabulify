a223e32ae2c569255961bd720f9554ed974ab5b4
Nico <gerardnico@gmail.com>
2024-05-29 19:49:48 +0200

    Backup on the go

--- ansible/roles/postgres/docker/docker/entrypoint/postgres-exporter-entrypoint.sh
+++ ansible/roles/postgres/docker/docker/entrypoint/postgres-exporter-entrypoint.sh
@@ -4,7 +4,7 @@
 # .bashrc to bring the connection environments
 . /root/.bashrc
 
-command="${POSTGRES_EXPORTER_ENV:-DATA_SOURCE_NAME='sslmode=disable' PG_EXPORTER_DISABLE_SETTINGS_METRICS=true} postgres_exporter ${POSTGRES_EXPORTER_FLAGS:---log.level=warn}"
+command="${POSTGRES_EXPORTER_ENV:-DATA_SOURCE_NAME='application_name=postgres_exporter sslmode=disable' PG_EXPORTER_DISABLE_SETTINGS_METRICS=true} postgres_exporter ${POSTGRES_EXPORTER_FLAGS:---log.level=warn}"
 printf "\nMetrics available at http://localhost:9187/metrics"
 printf "\nExecuting the command:\n%s\n" "$command"
 eval "$command"
