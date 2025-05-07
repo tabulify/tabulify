d5d80fc86c029d52c8cb0017ef38f52609bae24a
Nicolas GERARD <gerardnico@gmail.com>
2025-05-07 12:06:02 +0200

    feat: added tabli-ide

--- contrib/README.md
+++ contrib/README.md
@@ -1,14 +1,21 @@
 # Contrib
 
 
+## Script
+
+* [doc-exec](script/doc-exec) - Run code in the code block of the [tabli website](../tabulify-cli/src/main/tabulify.com)
+  and update the console block
+* [install-cli](script/install-cli) - Create a client distribution and installs it locally
+* [release](script/release) - Run all test, and creates a tag if successful
+* [tabli ide](script/tabli-ide) - Change your code, run `tabli-ide` and see your changes
+* [ryuk-stop](script/ryuk-stop) - Stops left-over [ryuk test container](https://github.com/testcontainers/moby-ryuk)
+  that should have stop but have not.
+
 ## Archive
+
 [archive](archive) contains old/archived artifact
 
 ## Doc
+
 [doc](doc) contains general dev documentation
 
-## Script
-
-* [install-cli](script/install-cli) - installs tabli locally
-* [release](script/release) - creates a release
-* [ryuk-stop](script/ryuk-stop) - stops left-over [ryuk test container](https://github.com/testcontainers/moby-ryuk) that should have stop but have not.
--- tabulify-cli/src/main/java/com/tabulify/tabli/TabliDiagnostic.java
+++ tabulify-cli/src/main/java/com/tabulify/tabli/TabliDiagnostic.java
@@ -2,6 +2,7 @@ package com.tabulify.tabli;
 
 import com.tabulify.Tabular;
 import com.tabulify.fs.FsFileManagerProvider;
+import com.tabulify.spi.ConnectionProvider;
 import com.tabulify.spi.DataPath;
 import net.bytle.cli.CliCommand;
 
@@ -16,10 +17,26 @@ public class TabliDiagnostic {
   public static List<DataPath> run(Tabular tabular, CliCommand childCommand) {
 
     System.out.println();
-    System.out.println("List of installed Tabulify file managers:");
+
     List<FsFileManagerProvider> fsFileManagerProviders = FsFileManagerProvider.installedProviders();
-    for (FsFileManagerProvider filemanagerProvider : fsFileManagerProviders) {
-      System.out.println(filemanagerProvider.getClass());
+    if (fsFileManagerProviders.isEmpty()) {
+      System.out.println("No Tabulify file managers could be found");
+    } else {
+      System.out.println("List of installed Tabulify file managers:");
+      for (FsFileManagerProvider filemanagerProvider : fsFileManagerProviders) {
+        System.out.println(filemanagerProvider.getClass());
+      }
+    }
+    System.out.println();
+
+    List<ConnectionProvider> installedProviders = ConnectionProvider.installedProviders();
+    if (installedProviders.isEmpty()) {
+      System.out.println("No Tabulify connection managers could be found");
+    } else {
+      System.out.println("List of installed Tabulify Connection managers:");
+      for (ConnectionProvider connectionProvider : installedProviders) {
+        System.out.println(connectionProvider.getClass());
+      }
     }
     System.out.println();
 
